1b456134f90409d44039c618ee3f9d853c232ec7
arthurdev <arthurmartins.dev2025@gmail.com>
2026-02-25 11:10:34 -0300

    chore: change vitest.config to exec tests without concurrent

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		fileParallelism: false,
+		sequence: {
+			concurrent: false,
+		},
 		globals: true,
 		environment: "node",
 		coverage: {
