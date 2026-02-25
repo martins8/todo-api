4f50e825363c952fa219f2c3b573f485bbff8853
arthurdev <arthurmartins.dev2025@gmail.com>
2026-02-25 10:24:26 -0300

    perf: improve code and perf in querys

--- src/services/todos.services.ts
+++ src/services/todos.services.ts
@@ -5,22 +5,16 @@ import type { TodoRequest, TodoResponse } from "../models/todos.models.js";
 export async function createTodo(todoData: TodoRequest): Promise<TodoResponse> {
 	const { title, description } = todoData;
 	try {
-		await client.execute({
-			sql: "INSERT INTO todos (title, description) VALUES (?, ?)",
+		const result = await client.execute({
+			sql: "INSERT INTO todos (title, description) VALUES (?, ?) RETURNING *",
 			args: [title, description],
 		});
-		const result = await client.execute({
-			sql: "SELECT * FROM todos WHERE id = LAST_INSERT_ROWID()",
-			args: [],
-		});
-		console.log("Created todo:", result.rows[0]);
 		const row = result.rows[0];
-		const todo: TodoResponse = {
+		return {
 			id: row.id as string,
 			title: row.title as string,
 			description: row.description as string,
 		};
-		return todo;
 	} catch (error) {
 		console.error("Error creating todo:", error);
 		throw error;
@@ -33,22 +27,17 @@ export async function updateTodo(
 ): Promise<TodoResponse> {
 	const { title, description } = todoData;
 	try {
-		await client.execute({
-			sql: "UPDATE todos SET title = ?, description = ? WHERE id = ?",
+		const result = await client.execute({
+			sql: "UPDATE todos SET title = ?, description = ? WHERE id = ? RETURNING *",
 			args: [title, description, id],
 		});
-		const result = await client.execute({
-			sql: "SELECT * FROM todos WHERE id = ?",
-			args: [id],
-		});
-		console.log("Updated todo:", result.rows[0]);
+		if (result.rows.length === 0) throw new NotFoundIdError({});
 		const row = result.rows[0];
-		const todo: TodoResponse = {
+		return {
 			id: row.id as string,
 			title: row.title as string,
 			description: row.description as string,
 		};
-		return todo;
 	} catch (error) {
 		console.error("Error updating todo:", error);
 		throw error;
@@ -57,20 +46,11 @@ export async function updateTodo(
 
 export async function deleteTodo(id: string): Promise<boolean> {
 	try {
-		const hasId = await client.execute({
-			sql: "SELECT id FROM todos WHERE id = ?",
+		const result = await client.execute({
+			sql: "DELETE FROM todos WHERE id = ? RETURNING id",
 			args: [id],
 		});
-
-		if (hasId.rows.length === 0) {
-			throw new NotFoundIdError({});
-		}
-
-		await client.execute({
-			sql: "DELETE FROM todos WHERE id = ?",
-			args: [id],
-		});
-
+		if (result.rows.length === 0) throw new NotFoundIdError({});
 		return true;
 	} catch (error) {
 		console.error("Error deleting todo:", error);
